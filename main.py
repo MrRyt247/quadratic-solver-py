85ab20fa5db6c0d50ce2036e4584d446833efba6
MrRyt247 <125740894+MrRyt247@users.noreply.github.com>
2024-02-01 14:18:59 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import openpyxl 
+import math
+
+df = pd.read_excel(r"C:\Users\HP\Desktop\input.xlsx")
+print(df)
+wb = openpyxl.load_workbook(r"C:\Users\HP\Desktop\input.xlsx")
+ws_input = wb['Input']
+count = 0
+
+def write_ans(x, y, i):
+    ws_input.cell(row=i, column=1, value = x)
+    ws_input.cell(row=i, column=2, value = y)
+
+def write_err(i):
+    ws_input.cell(row=i, column=1, value = 'This equation has complex roots')
+
+def calculate_roots(a, b, c):
+    if a == 0:                              # Verify quadratic equation
+        print("Not a quadratic equation. a cannot be zero")
+        return
+    
+    determinant = (b)**2 - 4 * a * c           # Calculate determinant\
+    count++
+    
+    if determinant >= 0:                    # Real roots calcaltion
+        x_one = (-b + math.sqrt(determinant)) / 2 * a
+        x_two = (-b - math.sqrt(determinant)) / 2 * a
+        write(x_one, x_two, count)
+    else:                                   # Complex roots error handling
+
+def read():
+    delete_sheet()
+    create_sheet()
+    for i, row in df.iterrows():
+        l = row.to_list()
+        a, b, c = l[0], l[1], l[2]
+        calculate_roots(a, b, c)
+read()
+
+def create_sheet():
+    wb.create_sheet('Output')
+    wb.save(r"C:\Users\HP\Desktop\input.xlsx")
+
+
+def delete_sheet():
+    wb.remove(wb['Output'])
+    wb.save(r"C:\Users\HP\Desktop\input.xlsx")
+
+    
