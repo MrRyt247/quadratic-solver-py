ce47403ca8414e8e00f4cdb0bed8b3d2086f2325
MrRyt247 <125740894+MrRyt247@users.noreply.github.com>
2024-02-01 21:00:38 +0000

    Finishing touches

--- main.py
+++ main.py
@@ -1,48 +1,48 @@
 import pandas as pd
-import openpyxl 
+import openpyxl
 import math
 
-df = pd.read_excel('input.xlsx')
-wb = openpyxl.load_workbook('input.xlsx')
-ws_output = wb['Output']
-count = 0
+wb = openpyxl.load_workbook('test.xlsx')            # Loads spreadsheet
+ws = wb['Output']
+count = 0                                           # Counts each written row
 
 def write_ans(x, y, i):
-    ws_output.cell(row=i, column=1, value = x)
-    ws_output.cell(row=i, column=2, value = y)
+    ws.cell(row = i, column = 1, value = x)
+    ws.cell(row = i, column = 2, value = y)
 
 def write_err(i):
-    ws_output.cell(row=i, column=1, value = 'This equation has complex roots')
+    ws.cell(row = i, column = 1, value = 'This equation has complex roots')
 
 def calculate_roots(a, b, c):
-    if a == 0:                              # Verify quadratic equation
+    if a == 0:                                     # Verify quadratic equation
         print("Not a quadratic equation. a cannot be zero")
-        return
-    
-    determinant = (b)**2 - 4 * a * c           # Calculate determinant\
-    count += 1
-    
-    if determinant >= 0:                    # Real roots calcaltion
-        x_one = (-b + math.sqrt(determinant)) / 2 * a
-        x_two = (-b - math.sqrt(determinant)) / 2 * a
-        write_ans(x_one, x_two, count)
-    else:                                   # Complex roots error handling
-        write_err(count)
-
-def read():
-    delete_sheet()
-    create_sheet()
-    for i, row in df.iterrows():
-        l = row.to_list()
-        a, b, c = l[0], l[1], l[2]
-        calculate_roots(a, b, c)
-    count = 0
-read()
+    else:   
+        determinant = (b)**2 - 4 * a * c           # Calculate determinant
+        global count                               # References global count variable
+        count += 1
+        
+        if determinant >= 0:                       # Calculates only real roots
+            x_one = (-b + math.sqrt(determinant)) / (2 * a)
+            x_two = (-b - math.sqrt(determinant)) / (2 * a)      
+            write_ans(x_one, x_two, count)
+        else:                                      # Complex roots error handling
+            write_err(count)
 
 def create_sheet():
     wb.create_sheet('Output')
-    wb.save('input.xlsx')
+    ws['A2'] = "x1"
+    ws['A2'] = "x2"
 
 def delete_sheet():
     wb.remove(wb['Output'])
-    wb.save('input.xlsx')
+
+def read():
+    delete_sheet()                                  # Resets previous calculations
+    create_sheet()
+    df = pd.read_excel('test.xlsx', sheet_name = 'Sheet1')     # Reads inputs
+    for i, row in df.iterrows():                    # Fetches values from each cell
+        l = row.to_list()
+        a, b, c = l[0], l[1], l[2]
+        calculate_roots(a, b, c)                    # Calculates for each row
+    wb.save('test.xlsx')
+read()
