ff10dfb9d35719ce75d730b7eae7d00484978430
MrRyt247 <125740894+MrRyt247@users.noreply.github.com>
2024-02-01 15:04:30 +0000

    update

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ import math
 df = pd.read_excel('input.xlsx')
 print(df)
 wb = openpyxl.load_workbook('input.xlsx')
-ws_input = wb['Input']
+ws_output = wb['Output']
 count = 0
 
 def write_ans(x, y, i):
-    ws_input.cell(row=i, column=1, value = x)
-    ws_input.cell(row=i, column=2, value = y)
+    ws_output.cell(row=i, column=1, value = x)
+    ws_output.cell(row=i, column=2, value = y)
 
 def write_err(i):
-    ws_input.cell(row=i, column=1, value = 'This equation has complex roots')
+    ws_output.cell(row=i, column=1, value = 'This equation has complex roots')
 
 def calculate_roots(a, b, c):
     if a == 0:                              # Verify quadratic equation
@@ -21,13 +21,14 @@ def calculate_roots(a, b, c):
         return
     
     determinant = (b)**2 - 4 * a * c           # Calculate determinant\
-    count++
+    count += 1
     
     if determinant >= 0:                    # Real roots calcaltion
         x_one = (-b + math.sqrt(determinant)) / 2 * a
         x_two = (-b - math.sqrt(determinant)) / 2 * a
         write(x_one, x_two, count)
     else:                                   # Complex roots error handling
+        write_err(count)
 
 def read():
     delete_sheet()
@@ -36,6 +37,8 @@ def read():
         l = row.to_list()
         a, b, c = l[0], l[1], l[2]
         calculate_roots(a, b, c)
+    count = 0
+
 read()
 
 def create_sheet():
